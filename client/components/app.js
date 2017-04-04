78e5809a6b4b9f0ca06acdd1a518f8face0d8b1a
Paolo Basso <paolo.basso87@gmail.com>
2017-04-04 17:57:00 +0200

    Initial commit

---
+++ client/components/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./header";
+
+const App = () => {
+
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    );
+}
+
+
+export default App;
---
+++ server/main.js
@@ -0,0 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import Bins from "../imports/collections/bins";
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});

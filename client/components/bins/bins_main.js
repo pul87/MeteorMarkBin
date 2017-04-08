2d38690dff4ddcfbb2d39d8416c39d7ad55a2f77
Paolo Basso <paolo.basso87@gmail.com>
2017-04-08 15:22:41 +0200

    Share Bin

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -4,6 +4,7 @@ import { Bins } from "../../../imports/collections/bins";
 
 import BinsEditor from "./bins_editor";
 import BinsViewer from "./bins_viewer";
+import BinsShare from "./bins_share";
 
 class BinsMain extends Component {
     render() {
@@ -15,6 +16,7 @@ class BinsMain extends Component {
             <div className="bins-main">
                 <BinsEditor bin={this.props.bin}/>
                 <BinsViewer bin={this.props.bin}/>
+                <BinsShare bin={this.props.bin}/>
             </div>
         );
     }
@@ -22,6 +24,7 @@ class BinsMain extends Component {
 
 export default createContainer((props) => {
     Meteor.subscribe("bins");
+    Meteor.subscribe("sharedBins");
     const bin = Bins.findOne(props.params.binId);
     return { bin };
 }, BinsMain);
--- imports/collections/bins.js
+++ imports/collections/bins.js
@@ -16,6 +16,10 @@ Meteor.methods({
 
     "bins.update": function(bin, newContent) {
         return Bins.update(bin._id, { $set: { content: newContent }});
+    },
+
+    "bins.share": function(bin, email) {
+        return Bins.update(bin._id, { $push: { sharedWith: email } });
     }
 });
 
